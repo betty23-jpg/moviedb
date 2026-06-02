a6b06a2924e734993c1b518542229a6278919a8f
betty23-jpg <kebedebetty23@gmail.com>
2026-06-02 07:58:04 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
 import Banner from './components/Banner/Banner'
